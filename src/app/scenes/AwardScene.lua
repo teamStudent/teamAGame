1b7268b311d495f6230eae7caa55a461d6dc69a6
Echo <1184203865@qq.com>
2016-03-08 11:13:32 +0800

    完成卡牌的随机可能出现和卡盘的翻转效果

--- src/app/scenes/AwardScene.lua
+++ src/app/scenes/AwardScene.lua
@@ -11,6 +11,7 @@ function AwardScene:ctor()
 end
 
 function AwardScene:init()
+
 	local bg = display.newSprite("AwardScene/bg.png")
 	bg:pos(display.cx, display.cy)
 	local scaleX = display.width/bg:getContentSize().width
@@ -18,36 +19,54 @@ function AwardScene:init()
 	bg:setScale(scaleX,scaleY)
 	self:addChild(bg)
 
+	local backBtn = cc.ui.UIPushButton.new({normal="back.png"},{scale9=true})
+	backBtn:onButtonClicked(function(event)
+		display.replaceScene(StartScene.new())
+	end)
+	backBtn:pos(display.left+50, display.top-50)
+	self:addChild(backBtn, 1)
+
+	math.randomseed(os.time())
+
+	for i=1,5 do
+	
 	local back = display.newSprite("AwardScene/back.png")
-	back:pos(display.cx-270, display.cy-150)
+	back:pos(display.cx-300, display.cy-150)
 	self:add(back)
 
-	local front = display.newSprite("AwardScene/kapai1.png")
-	front:pos(display.cx-270, display.cy-150)
+	local front = display.newSprite("AwardScene/kapai"..math.random(1,6)..".png")
+	front:pos(display.cx-300+150*(i-1), display.cy-150)
 	front:setVisible(false)
 	self:add(front)
 
-	
-	local seq1=cc.Sequence:create(cc.DelayTime:create(0.5),
+	back:setTouchMode(cc.TOUCH_MODE_ONE_BY_ONE)
+	back:setTouchEnabled(true)
+	back:addNodeEventListener(cc.NODE_TOUCH_EVENT, function(event)
+
+		    if event.name == "began" then
+
+        	local seq1=cc.Sequence:create(cc.DelayTime:create(0.5),
 								  cc.Show:create(),
 								  cc.DelayTime:create(1.0),
 								  cc.Hide:create())
-	local orbit1 = cc.OrbitCamera:create(2.0,1,0,0,360,0,0)
+
+			local orbit1 = cc.OrbitCamera:create(2.0,1,0,0,360,0,0)
 	
-	local  seq2=cc.Sequence:create(cc.DelayTime:create(0.5),
+			local  seq2=cc.Sequence:create(cc.DelayTime:create(0.5),
 								   cc.Hide:create(),
 								   cc.DelayTime:create(1.0),
 								   cc.Show:create())
 
 
-	local orbit2 = orbit1:clone()
+			local orbit2 = orbit1:clone()
 
-	back:runAction(cc.RepeatForever:create(cc.Spawn:create(orbit1,seq1)))
+			back:runAction(cc.Spawn:create(orbit1,seq1))
+			front:runAction(cc.Spawn:create(orbit2,seq2))
 
-	front:runAction(cc.RepeatForever:create(cc.Spawn:create(orbit2,seq2)))
-
-
-
+        return true
+    end
+end)
+	end
 end
 
 function AwardScene:onEnter()

f6ca93d91fff6df567eda280c101479adc3997dc
lixiaojun12 <297534880@qq.com>
2016-03-10 09:25:46 +0800

    no message

--- src/app/scenes/StartScene.lua
+++ src/app/scenes/StartScene.lua
@@ -11,7 +11,7 @@ function StartScene:ctor(  )
 end
 
 function StartScene:init()
-	local bg = display.newSprite("StartScene/bg.png")
+	local bg = display.newSprite("StartScene/startbg.png")
 	local scaleX = display.width/bg:getContentSize().width
 	local scaleY = display.height/bg:getContentSize().height
 	bg:setScale(scaleX,scaleY)
@@ -20,14 +20,23 @@ function StartScene:init()
 
 
     
-	self._startButton = cc.ui.UIPushButton.new({normal="StartScene/play.png"},{scale9=true})
+	self._startButton = cc.ui.UIPushButton.new({normal="StartScene/Up.png"},{scale9=true})
 	                   :onButtonClicked(function(event)
                    	   display.replaceScene(SelectScene.new())
                        end)
                        :pos(display.cx, display.cy-30)
                        :addTo(self)
-                       :setScale(1.5)
+                       :setScale(1.0)
                        
+      local tittl=display.newSprite("StartScene/selectplane_plane3_title.png")
+        tittl:setScale(2.0)
+        :pos(display.cx,display.cy/2*3)
+        :addTo(self)
+
+        local move1=cc.MoveBy:create(0.5,cc.p(0,10))
+        local move2=cc.MoveBy:create(0.5,cc.p(0,-10))
+        local sequence1=cc.Sequence:create(move1,move2)
+        transition.execute(tittl,cc.RepeatForever:create(sequence1))
 
 	
 	local music = cc.ui.UICheckBoxButton.new({off ="sound_off.png" ,on = "sound_on.png"})
